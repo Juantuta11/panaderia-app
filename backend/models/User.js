8917859c7501a6b35d339c2dcd330b12805918cb
MairayJuan <mairadaza84@gmail.com>
2025-07-10 14:20:46 -0500

    fix: ajusta modelo User para backend funcional

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-=======
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -24,23 +14,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
->>>>>>> daff2edabc7ece5e3685bcd4856c4c51fbde08b8
 }, {
   timestamps: true
 });
 
-<<<<<<< HEAD
 userSchema.set('toJSON', {
   transform: (doc, ret) => {
-    delete ret.password;
+    delete ret.password; 
     return ret;
   }
 });
 
 module.exports = mongoose.model('User', userSchema);
-
-
-
-=======
-module.exports = mongoose.model('User', userSchema);
->>>>>>> daff2edabc7ece5e3685bcd4856c4c51fbde08b8
